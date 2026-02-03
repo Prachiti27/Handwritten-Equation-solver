a63e823f3b4f298b561f4b2a8b4dc8775282db1c
Prachiti Kitey <prachitikitey86@gmail.com>
2026-02-03 22:20:33 +0530

    feat: add canvas image upload pipeline and FastAPI predict endpoint

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from handwritten-equation-solver!")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import base64
 
+app = FastAPI(title="Handwritten Equation Solver ")
 
-if __name__ == "__main__":
-    main()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class ImagePayload(BaseModel):
+    image: str
+
+@app.get("/health")
+def health_check():
+    return {"status": "ok"}
+
+@app.post("/predict")
+def receive_image(payload: ImagePayload):
+    image_data = payload.image.split(",")[1]
+    image_bytes = base64.b64decode(image_data)
+    
+    with open("received.png","wb") as f:
+        f.write(image_bytes)
+        
+    return {"message": "Image received successfully."}
+    
